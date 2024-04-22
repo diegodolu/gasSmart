e42e6e4a37115e30437734841acd88b3129cad20
laronis <linda.aroni@tecsup.edu.pe>
2024-04-21 22:48:59 -0500

    Sensor red y detalles

--- app/src/main/java/com/miempresa/gasapp/BoundActivity.kt
+++ app/src/main/java/com/miempresa/gasapp/BoundActivity.kt
@@ -1,11 +1,18 @@
 package com.miempresa.gasapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import android.widget.ImageView
 class BoundActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bound_app)
+
+        val icon = findViewById<ImageView>(R.id.img_bound_check)
+        icon.setOnClickListener{
+            val intent=Intent(this, RedsensorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
